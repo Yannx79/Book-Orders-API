bb563ebadc65634ca56acfb5ab41f9bd9b2a332a
Yannick <yannfscz@gmail.com>
2024-05-09 10:23:42 -0500

    feat: controllers

--- src/main/java/com/yfunes/bookordersapi/services/IBookService.java
+++ src/main/java/com/yfunes/bookordersapi/services/IBookService.java
@@ -1,6 +1,11 @@
 package com.yfunes.bookordersapi.services;
 
 import com.yfunes.bookordersapi.entities.BookEntity;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 public interface IBookService extends ICRUD<BookEntity, Integer> {
+
+    Page<BookEntity> findByPage(Pageable pageable);
+
 }
